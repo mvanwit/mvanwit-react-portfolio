e52170a5168fc481513ac424c6492bccbe525f7a
Matthieu Van Wittenberghe <mvanwit@gmail.com>
2021-11-08 15:17:24 +0100

    add About Section

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+	render() {
+		return(
+			<section id='about'>
+				<h2>Qui suis-je?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut varius enim, vitae vestibulum nunc. Mauris justo nunc, vulputate iaculis lectus consequat, auctor sagittis sapien. Aliquam luctus imperdiet neque sit amet ullamcorper. Vestibulum quis interdum ante. Morbi tincidunt lorem a aliquet scelerisque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus lobortis sit amet neque id blandit. Proin nec lectus vel leo consectetur gravida quis nec quam. Nullam suscipit blandit arcu ut porta. </p>
+			</section>
+		);
+	}
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './Nav.js';
 import Header from './Header.js';
+import About from './About.js';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+      <About />
     </div>
   );
 }
