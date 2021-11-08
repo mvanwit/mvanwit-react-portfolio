9464e3e6689ae54f291d5e8f598745026c1a5af9
Matthieu Van Wittenberghe <mvanwit@gmail.com>
2021-11-08 14:34:24 +0100

    add Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './Nav.js';
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Header />
     </div>
   );
 }
