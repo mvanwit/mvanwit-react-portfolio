307cc7670ef8088c76d2d60e47d43ac7da5649f2
mvanwit <mvanwit@gmail.com>
2021-12-09 11:23:29 +0100

    Add button go down and Header css

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 
@@ -10,12 +11,19 @@ class Header extends React.Component {
 			<header>
 				<h1>Matthieu Van Wittenberghe</h1>
 				<p>Développeur Web</p>
-				<a id ='dlcv' href='#' target='_blank'>
-					Mon CV
-					<br></br>
-					<br></br>
-					<FontAwesomeIcon icon={faFilePdf} size='3x'/>
-				</a>
+				<div>
+					<a id='dlcv' href='#' target='_blank'>
+						Mon CV
+						<br></br>
+						<br></br>
+						<FontAwesomeIcon icon={faFilePdf} size='3x'/>
+					</a>
+				</div>
+				<div>
+					<a id='godown' href='#about'>
+						<FontAwesomeIcon icon ={faAngleDown} size='4x'/>
+					</a>
+				</div>
 			</header>
 		);
 	}
